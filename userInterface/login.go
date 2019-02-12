891416ac8be2b9b1ef2bfac84c32cd3c38d48c2b
Travis <tgolnoft@gmail.com>
2019-02-12 16:24:29 -0700

    commiting other file, fixed trimming issue

--- userInterface/login.go
+++ userInterface/login.go
@@ -1,33 +1,27 @@
 package userInterface
 
 import (
-    "fmt"
-    "bufio"
-    "os"    
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func GetUsername() (un string) {
-    fmt.Print("Enter thy username: ")
-    
-    reader := bufio.NewReader(os.Stdin)
-    un, _ = reader.ReadString('\n')
-    return
+	fmt.Print("Enter thy username: ")
+	reader := bufio.NewReader(os.Stdin)
+	un, _ = reader.ReadString('\n')
+	return
 }
 
-func GetPassword() (pw string){
-    fmt.Print("Enter thy password: ")
-    reader := bufio.NewReader(os.Stdin)
-    pw, _ = reader.ReadString('\n')
-    return
+func GetPassword() (pw string) {
+	fmt.Print("Enter thy password: ")
+	reader := bufio.NewReader(os.Stdin)
+	pw, _ = reader.ReadString('\n')
+	return
 }
 
 func GetUsernameAndPassword() (un, pw string) {
-    un = trim(GetUsername())
-    pw = trim(GetPassword())
-    return
-}
-
-func trim(s string) string {
-    return strings.Replace(s, "\n", "", -1)
+	un = GetUsername()
+	pw = GetPassword()
+	return un, pw
 }
