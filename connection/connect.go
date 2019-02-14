001a152c74178a3cfd9cd6f5445bbe90835135ec
Travis <tgolnoft@gmail.com>
2019-02-14 14:02:50 -0700

    fixed my mistakes ...

--- connection/connect.go
+++ connection/connect.go
@@ -1,2 +1,28 @@
 package connect
 
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+type Connection struct{
+	Con net.Conn
+}
+
+func(connect Connection) Open(port string, host string) {
+	connection, err := net.Dial("tcp", host+":"+port)
+	if err != nil {
+		fmt.Println("Error in Client main: " + err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Connected")
+	connect.Con = connection
+}
+
+func(connect Connection) Close(){
+	connect.Con.Close()
+	fmt.Println("Connection Closed")
+}
+
+
