4a8f9ac7712499a64b79890110dd7ab187aa51ee
Travis <tgolnoft@gmail.com>
2019-02-14 13:13:59 -0700

    some more touchup changes

--- userInterface/login.go
+++ userInterface/login.go
@@ -7,14 +7,14 @@ import (
 )
 
 func GetUsername() (un string) {
-	fmt.Print("Enter thy username: ")
+	fmt.Print("Enter your username: ")
 	reader := bufio.NewReader(os.Stdin)
 	un, _ = reader.ReadString('\n')
 	return
 }
 
 func GetPassword() (pw string) {
-	fmt.Print("Enter thy password: ")
+	fmt.Print("Enter your password: ")
 	reader := bufio.NewReader(os.Stdin)
 	pw, _ = reader.ReadString('\n')
 	return
@@ -23,5 +23,5 @@ func GetPassword() (pw string) {
 func GetUsernameAndPassword() (un, pw string) {
 	un = GetUsername()
 	pw = GetPassword()
-	return un, pw
+	return
 }
