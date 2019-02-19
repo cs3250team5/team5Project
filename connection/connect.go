c7c822a6610a40db62af808a1c9530422cbad8e5
Travis <tgolnoft@gmail.com>
2019-02-19 14:16:23 -0700

    first pass read/write functionality

--- connection/connect.go
+++ connection/connect.go
@@ -6,11 +6,11 @@ import (
 	"os"
 )
 
-type Connection struct{
+type Connection struct {
 	Con net.Conn
 }
 
-func(connect Connection) Open(port string, host string) {
+func (connect Connection) Open(host string, port string) {
 	connection, err := net.Dial("tcp", host+":"+port)
 	if err != nil {
 		fmt.Println("Error in Client main: " + err.Error())
@@ -20,9 +20,20 @@ func(connect Connection) Open(port string, host string) {
 	connect.Con = connection
 }
 
-func(connect Connection) Close(){
+func (connect Connection) write(s string) {
+	fmt.Fprintf(connect.Con, s)
+}
+
+func (connect Connection) read() (string, error) {
+	buffer := make([]byte, 1024)
+	numBytes, err := connect.Con.Read(buffer)
+	if err != nil && err.Error() != "EOF" {
+		return "", err
+	}
+	return string(buffer[:numBytes]), nil
+}
+
+func (connect Connection) Close() {
 	connect.Con.Close()
 	fmt.Println("Connection Closed")
 }
-
-
