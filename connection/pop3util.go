3a8c159a998db100f16f6c85c99fa22bd38e63f5
Frederick Buker <frederickbuker@gmail.com>
2019-02-26 15:14:37 -0700

    auth attempt

--- connection/pop3util.go
+++ connection/pop3util.go
@@ -1,23 +1,14 @@
 package connection
 
-import ( 
-	"fmt"
-	"encoding/hex"
-	)
-	
+import(
+
+ "fmt"
+ 
+ )
+
 func Pop3Auth(host string, port string, un string, pw string) {
 	conn := MakeConnection(host, port)
 	s, _ := conn.Read()
 	fmt.Println(s)
 	conn.Close()
 }
-
-func Interpret(mess string) string{
-	var a string 
-	a, err =  hex.DecodeString(mess)// turns the string into a series of bytes in ascii format
-	if err != nil{
-		panic(err)
-	}
-	return string(a)//returns the bytes stream translated into ascii 
-}
-
