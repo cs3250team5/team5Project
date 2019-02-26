a2f389931a18410d7d712340c5744991fafb91a2
Frederick Buker <frederickbuker@gmail.com>
2019-02-26 15:38:45 -0700

    auth

--- connection/pop3util.go
+++ connection/pop3util.go
@@ -10,5 +10,13 @@ func Pop3Auth(host string, port string, un string, pw string) {
 	conn := MakeConnection(host, port)
 	s, _ := conn.Read()
 	fmt.Println(s)
+	
+	conn.Write("USER " + un + "\r\n")
+	s1 ,_ := conn.Read()
+	fmt.Println(s1)
+	conn.Write("PASS " + pw + "\r\n")
+	s2 ,_ := conn.Read()
+	fmt.Println(s2)
+	
 	conn.Close()
 }
