9b7986c55bd472841412bc5b0c7adda12e11ecf9
Frederick Buker <frederickbuker@gmail.com>
2019-02-27 23:45:41 -0700

    auth  with list and retr

--- connection/pop3util.go
+++ connection/pop3util.go
@@ -6,7 +6,8 @@ import(
  
  )
 
-func Pop3Auth(host string, port string, un string, pw string) {
+func Pop3Auth(host string, port string, un string, pw string)*Connection {
+	
 	conn := MakeConnection(host, port)
 	s, _ := conn.Read()
 	fmt.Println(s)
@@ -18,5 +19,35 @@ func Pop3Auth(host string, port string, un string, pw string) {
 	s2 ,_ := conn.Read()
 	fmt.Println(s2)
 	
+	conn.Write("LIST" + "\r\n")
+	list ,_ := conn.Read()
+	fmt.Println(list)
+	
+	conn.Write("RETR" + "\r\n")
+	retr ,_ := conn.Read()
+	fmt.Println(retr)
+
 	conn.Close()
+	
+	return conn
 }
+/*
+func Pop3List(conn *Connection) string{
+	
+	
+	conn.Write("LIST" + "\r\n")
+	list ,_ := conn.Read()
+	fmt.Println(list)
+	
+	return "list"
+}
+
+func Pop3Retr(conn *Connection) string{
+
+	conn.Write("RETR" + "\r\n")
+	retr ,_ := conn.Read()
+	fmt.Println(retr)
+	
+	return "retr"	
+}
+*/
