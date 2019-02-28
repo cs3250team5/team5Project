6b6c1019f23e774f9d55416e98af2768b637627f
Travis <tgolnoft@gmail.com>
2019-02-27 20:41:49 -0700

    first upgrade to pop

--- connection/pop3util.go
+++ connection/pop3util.go
@@ -1,10 +1,37 @@
 package connection
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Pop3Auth(host string, port string, un string, pw string) {
 	conn := MakeConnection(host, port)
+	defer conn.Close()
 	s, _ := conn.Read()
-	fmt.Println(s)
-	conn.Close()
+	if check, _ := StringFirstRest(s); check != "+OK" {
+		fmt.Println("Login Failed")
+		return
+	}
+	conn.Write("USER " + un + "\r\n")
+
+	s1, _ := conn.Read()
+	if check, _ := StringFirstRest(s1); check != "+OK" {
+		fmt.Println("Login Failed")
+		return
+	}
+	conn.Write("PASS " + pw + "\r\n")
+
+	s2, _ := conn.Read()
+	if check, _ := StringFirstRest(s2); check != "+OK" {
+		fmt.Println("Login Failed")
+		return
+	}
+	fmt.Println("Login Succeeded")
+}
+
+func StringFirstRest(s string) (string, string) {
+	first := strings.Fields(s)[0]
+	rest := s[len(first)+1:]
+	return first, rest
 }
