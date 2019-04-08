32787a79a23a3ba792b7fcdb8b0272e46ea45674
steven hanson <s.ahanson@yhaoo.com>
2019-04-08 15:37:59 -0600

    classes

--- smtp/profile.go
+++ smtp/profile.go
@@ -1,5 +1,19 @@
 package smtp
 
+import(
+	"fmt"
+	"io/ioutil"
+	"connection"
+	
+)
+type Profile struct{
+	var Name,Address,Sgun,Sgpw string
+}
+
+type Smail struct{
+	var To,Sub,Mess string
+}
+
 func SmtpLogin(){
 
 
