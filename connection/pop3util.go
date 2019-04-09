2b094199e017643f66d555388508daf22e1e90b1
Alfredo Marroquin <amarroq2@msudenver.edu>
2019-04-09 15:41:47 -0600

    consolidating retrieve func

--- connection/pop3util.go
+++ connection/pop3util.go
@@ -58,3 +58,16 @@ func Pop3Retr(conn *Connection, msgNo int, byteNo int) string {
 	//fmt.Printf("Retrieved %d\n%s\n", msgNo, s)
 	return s
 }
+
+func RetrieveAll(conn *Connection, listMap map[int]int) map[int]MailObject {
+	// Emails are put into maps based on data size
+	finalMap := make(map[int]MailObject)
+	for key, value := range listMap {
+		s := Pop3Retr(conn, key, value)
+		mail := InterpretLines(s)
+		mail.Num = key
+		finalMap[key] = mail
+		fmt.Println("\nMessage", key, "\n", mail.To)
+	}
+	return finalMap
+}
