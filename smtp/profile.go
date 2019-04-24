5494d442497960581c670ebfab7c704b677ba9ef
Frederick Buker <frederickbuker@gmail.com>
2019-04-24 17:36:22 -0600

    This only can send email through the emailclient

--- smtp/profile.go
+++ smtp/profile.go
@@ -1,71 +1,81 @@
 package smtp
 
-import(
+import (
+	"Team5Project/connection"
+	"Team5Project/userInterface"
+	//"Team5Project/util"
+	//"bufio"
 	"fmt"
-	"io/ioutil"
-	"connection"
+	//"path/filepath"
 	"net/smtp"
-	
+	//"os"
+	//"regexp"
+	//"strings"
 )
 
-func EmailTo() string {
-	
-	// user writes message to 
-	fmt.Print("To: ")
-	reader := bufio.NewReader(os.Stdin)
-	to, _ := reader.ReadString('\n') 
-	return strings.TrimSpace(to)
-}
-
-func EmailSubject() string {
-	
-	// user tpyes subject of email
-	fmt.Print("Subject: ")
-	reader := bufio.NewReader(os.Stdin)
-	sub, _ := reader.ReadString('\n')
-	return strings.TrimSpace(sub)
-}
-
-func EmailMsg() string {
-	
-	// User types message
-	fmt.Print("Write message")
-	reader := bufio.NewReader(os.Stdin)
-	wmsg, _ := reader.ReadString('\n')
-	fmt.Print(".\n")
-	return strings.TrimSpace(emsg)
-}
-
-
-
-func SendMail(conn *Connection, EmailTo string, EmailSubject string, EmailMsg string){
-	
-	sub := composeEmail.EmailSubject()
-	writeMsg := composeEmail.EmailMsg()
-
-	//configuration
-	hostURL := "smtp.gmail.com"//This will change
-	hostPORT := "587"// This will change
-	emailSender := "fbuker@nyx.net"
-	password := "su$lpHUr.86097"
-	emailReciever := composeEmail.EmailTo()
-
-	//creating auth object
-	emailAUTH := smtp.PlainAuth(
-		"", emailSender, password, hostURL)
+func SendMail(mail connection.MailObject, EmailTo string, EmailSubject string, EmailMsg string) {
 
 	//creating the email
-	msg := []byte("To: " + emailReciever + "\r\n" + "Subject :" + sub + "\r\n" +  writeMsg)
+	sub := userInterface.EmailSubject()
+	writeMsg := userInterface.EmailMsg()
+	emailReciever := userInterface.EmailTo()
 
+	//configuration
+	hostURL := "smtp.gmail.com" //This will change
+	hostPORT := "587"           // This will change
+	emailSender := "cs3250Team5Relay"
+	password := "bdexlpeeudlnsuwy"
+	
+	//creating auth object
+	emailAUTH := smtp.PlainAuth("", emailSender, password, hostURL)
+	msg := []byte("To: " + emailReciever + "\r\n" + "Subject :" + sub + "\r\n" + writeMsg)
+/*
+	//user decides whether to save and send
+	fmt.Print("Would you like save as a draft or send the message?// S/s D/d")
+	reader := bufio.NewReader(os.Stdin)
+	choice, _ := reader.ReadString('\n')
+	fmt.Print(".\n")
+	fmt.Println(choice)
 	//send the mail
-	err := smtp.SendMail(
-		hostURL+":"+hostPORT, emailAUTH, emailSender, []string{emailReciever}, msg)
+	if choice == "S" || choice == "s" {*/
+		err := smtp.SendMail(
+			hostURL+":"+hostPORT, emailAUTH, emailSender, []string{emailReciever}, msg)
 
-	if err != nil {
-		fmt.Print("Error :", err)
+		if err != nil {
+			fmt.Print("Error :", err)
 	}
 	fmt.Println(" email sent from " + emailReciever)
+	/*}
+	if choice == "D" || choice == "d" {
+		var findLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
 
-	connect.Con.Close()
+		if findLetter(EmailTo) == true || findLetter(EmailSubject) == true || findLetter(EmailMsg) == true {
+
+			fileName := fmt.Sprintf("%d_%s_%s.txt", mail.Num, mail.Subject, util.CleanFrom(mail.From))
+			fileName = strings.Replace(fileName, " ", "_", -1)
+			dir, err := filepath.Abs("draft")
+			check(err)
+
+			f, err := os.Create(filepath.Join(dir, filepath.Base(fileName))) //creates file within local Draft folder
+			check(err)
+
+			defer f.Close()
+			d := fmt.Sprintf("Num: %d\nTo: %s\nFrom: %s\nDate: %s\nSubject: %s\nMessage:\n%s\n", mail.Num, mail.To, mail.From, util.CleanDate(mail.Date), mail.Subject, mail.Message)
+			f.Write([]byte(d))
+			g := ("Draft saved and draft folder made.")
+			fmt.Println(msg, emailAUTH)
+			return g/*
+
+		}
+	}*/
+
+	/*connect.Con.Close()*/
+
 }
 
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
