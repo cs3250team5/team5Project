dc1e4eee204ae4698075feabbcc10ceeb217c41a
steven hanson <s.ahanson@yhaoo.com>
2019-05-01 15:13:22 -0600

    emailsending

--- smtp/EmailSending.go
+++ smtp/EmailSending.go
@@ -1,23 +1,29 @@
 package smtp
 
 import (
-	"Team5Project/connection"
+	//"Team5Project/connection"
+	"Team5Project/userInterface"
 	//"Team5Project/util"
-	//"bufio"
+	"bufio"
 	"fmt"
-	//"path/filepath"
+	"path/filepath"
 	"net/smtp"
-	//"os"
-	//"regexp"
-	//"strings"
+	"os"
+	"regexp" 
+	"strings"
 )
+type MailDraft struct {
+	To, Subject, Message string
+}
 
-func SendMail(mail connection.MailObject, EmailTo string, EmailSubject string, EmailMsg string) {
+
+func ComposeSend(/*mail connection.MailObject,*/ draft MailDraft, EmailTo string, EmailSubject string, EmailMsg string) {
 
 	//creating the email
 	emailReciever := userInterface.EmailTo()
 	sub := userInterface.EmailSubject()
 	writeMsg := userInterface.EmailMsg()
+
 
 	//configuration
 	hostURL := "smtp.gmail.com" //This will change
@@ -28,27 +34,27 @@ func SendMail(mail connection.MailObject, EmailTo string, EmailSubject string, E
 	//creating auth object
 	emailAUTH := smtp.PlainAuth("", emailSender, password, hostURL)
 	msg := []byte("To: " + emailReciever + "\r\n" + "Subject :" + sub + "\r\n" + writeMsg)
-/*
+
 	//user decides whether to save and send
 	fmt.Print("Would you like save as a draft or send the message?// S/s D/d")
 	reader := bufio.NewReader(os.Stdin)
 	choice, _ := reader.ReadString('\n')
-	fmt.Print(".\n")
 	fmt.Println(choice)
 	//send the mail
-	if choice == "S" || choice == "s" {*/
-		err := SendMail(hostURL+":"+hostPORT, emailAUTH, emailSender, []string{emailReciever}, msg)
+	if strings.HasPrefix(choice,"s") || strings.HasPrefix(choice,"S"){
+		err := smtp.SendMail(hostURL+":"+hostPORT, emailAUTH, emailSender, []string{emailReciever}, msg)
+
 		if err != nil {
 			fmt.Print("Error :", err)
+		}
+		fmt.Println(" email sent to " + emailReciever)
 	}
-	fmt.Println(" email sent to " + emailReciever)
-	/*}
-	if choice == "D" || choice == "d" {
+	if strings.HasPrefix(choice,"d") || strings.HasPrefix(choice,"D"){
 		var findLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
 
 		if findLetter(EmailTo) == true || findLetter(EmailSubject) == true || findLetter(EmailMsg) == true {
 
-			fileName := fmt.Sprintf("%d_%s_%s.txt", mail.Num, mail.Subject, util.CleanFrom(mail.From))
+			fileName := fmt.Sprintf("%s.txt", "new draft")
 			fileName = strings.Replace(fileName, " ", "_", -1)
 			dir, err := filepath.Abs("draft")
 			check(err)
@@ -57,17 +63,17 @@ func SendMail(mail connection.MailObject, EmailTo string, EmailSubject string, E
 			check(err)
 
 			defer f.Close()
-			d := fmt.Sprintf("Num: %d\nTo: %s\nFrom: %s\nDate: %s\nSubject: %s\nMessage:\n%s\n", mail.Num, mail.To, mail.From, util.CleanDate(mail.Date), mail.Subject, mail.Message)
+			d := fmt.Sprintf("%s", msg)
 			f.Write([]byte(d))
-			g := ("Draft saved and draft folder made.")
-			fmt.Println(msg, emailAUTH)
-			return g/*
-
+			//fmt.Println(msg, emailAUTH)
+			/*
+			if err != nil {
+			fmt.Print("Error :", err)
+			}*/
+		
 		}
-	}*/
-
+	}
 	/*connect.Con.Close()*/
-
 }
 
 
